0c57b3a0b6647ff5366a338343e6f0c806ba2c9f
Taazim Temirova <tezi.temirova@gmail.com>
2020-04-13 16:10:41 +0600

    created controller and added html,css,js files

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Controller {
+    Map<String, Object> userMap = new HashMap<>();
+
+    public Controller() {
+        // init user Map
+        userMap.put("name", "Aibek");
+        userMap.put("surname", "Askarov");
+        userMap.put("gender", "MALE");
+        userMap.put("birthDate", LocalDate.of(1990, 1, 1));
+        userMap.put("graduated", true);
+        userMap.put("gpa", 3.9);
+
+    }
+    @GetMapping("/demo/getUser")
+    public Map<String, Object> getUser() {
+        return this.userMap;
+    }
+
+    @GetMapping("/demo")
+    public String demo(Model model) {
+        return "index";
+    }
+
+    @PostMapping("/demo/post")
+    public String postDemo(@RequestParam("login") String login, @RequestParam("password") String password) {
+        System.out.println(login + " - " + password);
+        return "redirect:/demo/";
+    }
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,235 @@
+
+'use strict';
+
+class User {
+  constructor(id, name, email, isAuthorised) {
+    this.id = id,
+    this.name = name,
+    this.email = email,
+    this.isAuthorised = isAuthorised;
+  }
+};
+class Comment {
+  constructor(user, post, comment) {
+    this.user = user,
+    this.post = post,
+    this.comment = comment;
+  } 
+};
+class Post {
+  constructor(id, userId, image, descriprtion) {
+    this.id = id,
+    this.userId = userId,
+    this.image =  image,
+    // так можно обращаться к конкретному юзеру
+    this.descriprtion = descriprtion;
+  }
+};
+//////////////////////////////////////////
+
+function authorize(user) {
+  user.isAuthorised = true;
+}
+const user = new User(1, "Anna", "anna@gmail.com", false);
+authorize(user);
+const post = new Post(1, user.id, "images/angryCat.jpg",  "Lorem ipsum dolor sit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque libero minima corporis accusamus nobis velit.");
+const user2 = new User(2, "Alex","gena@gena.com", false);
+authorize(user2);
+const comment = new Comment(user2.name, post.id, "Kitty you are so cute! Don't be so angry)) with love<3");
+
+console.log(user);
+console.log(post);
+console.log(user2);
+console.log(comment);
+
+let posts = [];
+
+function newPost(i) {
+  return new Post(i+2, user.id,  "text" + i, 0);
+};
+
+for(let i = 0; 6 > i; i++) {
+  posts[i] = newPost(i);
+};
+
+function toPrint(post) {
+  console.log(post);
+};
+posts.forEach(toPrint);
+
+function like (post, postId, isLiked) {
+  if(post.id === postId) {
+    isLiked ? post.likes++ : post.likes--;
+    console.log(post);
+  };
+};
+
+let postId = 4;
+
+let isLiked = true;
+
+for(let i = 0; i < posts.length; i++) {
+  like(posts[i], postId, isLiked);
+};
+
+
+//hiding and showing splash page and no-scroll
+
+  const signIn= document.getElementById('button');
+  signIn.addEventListener('click', hideSplashScreen);
+
+  
+function showSplashScreen(){
+  document.getElementById('page-splash').hidden = false;
+	document.body.classList.add('no-scroll');
+}
+
+// showSplashScreen();
+
+function hideSplashScreen(){
+    document.getElementById('page-splash').hidden = true;
+	document.body.classList.remove('no-scroll');
+}
+
+function createCommentElement(comment){
+    let elem= document.createElement('div')
+    elem.classList.add('py-2');
+    elem.classList.add('pl-3');
+    elem.innerHTML = '<a href="#" class="muted">'  + comment.user + '</a>' + 
+  '<p>' + comment.comment + '</p>';
+  return elem;
+}
+
+function createPostElement(post){
+  let elem = document.createElement('div');
+  elem.classList.add('card');
+  elem.classList.add('my-3');
+  elem.innerHTML = 
+  '<div id= "cat2" >' +
+    '<img class="d-block w-100" src="' + post.image + '" alt="Post image">' +
+  '</div>' +
+    '<div class="px-4 py-3">' +
+    '<div id = "option2" class="d-flex justify-content-around">' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-heart"></i>' +
+      '</span>'
+       +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-comment"></i>' +
+      '</span>' +
+      '<span class="mx-auto"></span>' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-bookmark"></i>' +
+      '</span>'
+       +
+    '</div>' +
+  '<hr>' +
+  '<div>' +
+    '<p>' + post.descriprtion + '</p>' +
+  '</div>' +
+  '<hr>' +
+  '<div id="comments" class="comments">' +
+  '<div class="py-2 pl-3">' +
+          '<a href="#" class="muted">' + 'someusername' + '</a>' +
+          '<p>' + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.' + '</p>' +
+        '</div>' +
+  '</div>' + '</div>';
+  return elem;
+}
+
+
+function addPost(postElement){
+    document.getElementById("posts-container").append(postElement);
+}
+
+let postsCont = createPostElement(post);
+addPost(postsCont);
+document.getElementById('comments').append(createCommentElement(comment));
+
+////////////////////////////////////// homework59
+
+const hearts = document.getElementsByClassName('fa-heart');
+for(let i = 0; i < hearts.length; i++) {
+  console.log(hearts[i]);
+    hearts[i].addEventListener('click', function() {
+        if(hearts[i].classList.contains('fas')) {
+            hearts[i].classList.remove('fas');
+            hearts[i].classList.remove('text-danger');
+            hearts[i].classList.add('far');
+        } else {
+            hearts[i].classList.remove('far');
+            hearts[i].classList.add('text-danger');
+            hearts[i].classList.add('fas');
+        }
+    })
+};
+
+const bookmark = document.getElementsByClassName('fa-bookmark');
+for(let i=0; i< bookmark.length; i++){
+  bookmark[i].addEventListener('click',function(){
+    if(bookmark[i].classList.contains('fas')){
+        bookmark[i].classList.remove('fas');
+        bookmark[i].classList.add('far');
+    } else {
+        bookmark[i].classList.remove('far');
+        bookmark[i].classList.add('fas');
+    }
+  })
+}
+
+const postss = document.getElementsByClassName('card');
+for(let i = 0; i < postss.length; i++) {
+    let ims = postss[i].getElementsByClassName('w-100');
+    let he = postss[i].getElementsByClassName('fa-heart')[0];
+    for(let j = 0; j < ims.length; j++) {
+        ims[j].addEventListener('dblclick', function() {
+          if(he.classList.contains('fas')) {
+            he.classList.remove('fas');
+            he.classList.remove('text-danger');
+            he.classList.add('far');
+           } else {
+            he.classList.remove('far');
+            he.classList.add('text-danger');
+            he.classList.add('fas');
+           }
+        })
+    }
+
+};
+
+
+// const comm = document.querySelectorAll('.comments')[0];
+// console.log(comm);
+// // for(let i = 0; i<comm.length; i++){
+//   comm.addEventListener('click', comText);
+//   // console.log(comm[i]);
+// // }
+
+// function comText(){
+//   let elem = document.createElement('div');
+//   elem.classList.add('col-lg-10');
+// elem.innerHTML = '<textarea rows="3" cols="3" class="form-control" placeholder="Добавьте комментарий"></textarea>';
+// // comm.append(elem);
+// return elem;
+// }
+// console.log(comText);
+
+document.querySelector('#btn').onclick = function(event){
+    event.preventDefault();
+    let form = document.querySelector('form');
+    console.log(form.elements.test.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
